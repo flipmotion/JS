a636ed8a214525be43a902b8086603e822ccd52e
Igor <igor@Igors-MacBook-Pro.local>
2017-03-10 20:49:17 +0300

    fix 0,1,-1

--- build/js/my/app.js
+++ build/js/my/app.js
@@ -7,22 +7,32 @@ Object.find = function(path, obj) {
 };
 
 const tripsNew = trips.slice(0);
+const tripsCustom = trips.slice(0);
 
 const compare = (a, b, reverse) => {
-  return reverse === 'ASC' ? a >= b : a <= b;
+  if(a === b || a === undefined || b === undefined) return 0;
+  if(reverse) {
+    return a > b ? -1 : 1;
+  } else {
+    return a > b ? 1 : -1;
+  }
 }
 
 const createComporator = (path, key) => (prev, current) => {
   return(compare(
     Object.find(path, prev),
     Object.find(path, current),
-    key, 
+    key,
   ))
 }
 
 
-trips.sort(createComporator('price.value', 'ASC'));
+trips.sort(createComporator('price.value', false));
 console.log(`Sort ASC: ${trips.map(item=>item.price.value)}`);
 
-tripsNew.sort(createComporator('price.value', 'DESC'));
+tripsNew.sort(createComporator('price.value', true));
 console.log(`Sort DESC: ${tripsNew.map(item=>item.price.value)}`);
+
+// tripsCustom.sort(createComporator('carrier-name'));
+// console.log(tripsCustom);
+//console.log(`Sort DESC: ${tripsCustom.map(item=>item.price.value)}`);
