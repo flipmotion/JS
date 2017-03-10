9aefc7650a7cd2595945a462542ad3c0bbe389cd
Igor <igor@Igors-MacBook-Pro.local>
2017-03-10 20:20:38 +0300

    sort ASC\DESC

--- build/js/my/app.js
+++ build/js/my/app.js
@@ -1,216 +1,28 @@
-"use strict";
+import trips from './trips';
 
-document.addEventListener('DOMContentLoaded', function(){  
+Object.find = function(path, obj) {
+  return path.split('.').reduce(function(prev, curr) {
+    return prev ? prev[curr] : undefined
+  }, obj || this);
+};
 
-  // function Btn(width, height) {
-  //   this.width = width || 50;
-  //   this.height = height || 50;
-  //   this.elm = null;
-  // }
+const tripsNew = trips.slice(0);
 
-  // Btn.prototype.render = function(whare){
-  //   if (this.elm) {
-  //     this.elm.style.width = this.width + 'px';
-  //     this.elm.style.height = this.height + 'px';
-  //   }
+const compare = (a, b, reverse) => {
+  return reverse === 'ASC' ? a >= b : a <= b;
+}
 
-  //   whare.appendChild(this.elm);
-  // }
-
-  // function button(width, height, text){
-  //   Btn.call(this, width, height);
-
-  //   this.text = text || 'Default';
-  //   var btn = document.createElement('BUTTON');
-  //   var label = document.createTextNode(text);
-
-  //   btn.appendChild(label);
-  //   this.elm = btn;
-  // }
-
-  // button.prototype = Object.create( Btn.prototype );
-  // // button.prototype.constructor = button;
-
-  // button.prototype.render = function(whare){
-  //   Btn.prototype.render.call(this, whare);
-
-  //   this.elm.addEventListener('click', this.onClick.bind(this), false);
-  // }
-
-  // button.prototype.onClick = function(e) {
-
-  //   var paragraph = document.createElement('P');
-  //   var label = document.createTextNode(this.text);
-  //   paragraph.appendChild(label);
-  //   document.body.appendChild(paragraph);
-  //   console.log(`button ${this.text} clicked`);
-  // }
-
-  // var btn1 = new button('100', '50', 'button 1');
-  // var btn2 = new button('130', '50', 'button 2');
-
-  // btn1.render(document.body);
-  // btn2.render(document.body);
-
-  //OOLO pattern
-
-  // var Btn = {
-  //   init: function(width, height) {
-  //     this.width = width;
-  //     this.height = height;
-  //     this.elm = null;
-  //   },
-
-  //   render: function(whare) {
-  //     if (this.elm) {
-  //       this.elm.style.width = this.width + 'px';
-  //       this.elm.style.height = this.height + 'px';
-  //     }
-  //     whare.appendChild(this.elm);
-  //   }
-  // }
-
-  // var Button = Object.create(Btn);
-
-  // Button.setup = function(width, height, text) {
-  //   this.init(width, height);
-  //   this.text = text || 'Default';
-
-  //   var btn = document.createElement('BUTTON');
-  //   var label = document.createTextNode(text);
-  //   btn.appendChild(label);
-
-  //   this.elm = btn;
-  // }
-
-  // Button.build = function(whare) {
-  //   this.render(whare);
-  //   this.elm.addEventListener('click', this.onClick.bind(this));
-  // }
-
-  // Button.onClick = function(e) {
-  //   console.log(this.text);
-  // }
-
-  // var btn1 = Object.create( Button );
-  // btn1.setup('120', '30', 'button 1');
-
-  // var btn2 = Object.create( Button );
-  // btn2.setup('200', '50', 'button 2');
-
-  // btn1.build(document.body);
-  // btn2.build(document.body);
+const createComporator = (path, key) => (prev, current) => {
+  return(compare(
+    Object.find(path, prev),
+    Object.find(path, current),
+    key, 
+  ))
+}
 
 
-  function Link(link) {
-    this.link = link;
-  }
+trips.sort(createComporator('price.value', 'ASC'));
+console.log(`Sort ASC: ${trips.map(item=>item.price.value)}`);
 
-  Link.prototype.getNode = function() {
-    var li = document.createElement('a');
-    li.style.cursor = 'pointer';
-    li.classList.add('list-group-item');
-    li.innerText = this.link;
-    return li;
-  }
-
-
-  function Menu(links) {
-    this.links = links.map(function(link){
-      return new Link(link);
-    });
-  }
-
-  Menu.prototype.getNode = function(){
-    var container = document.createElement('div');
-    container.classList.add('list-group');
-
-    this.links.forEach(function(link){
-      container.appendChild(link.getNode());
-    });
-
-    return container;
-  }
-
-  function ContextMenu(links) {
-    Menu.call(this, links);
-    this.container = this.render();
-    this.listener();
-  }
-
-  ContextMenu.prototype = Object.create( Menu.prototype );
-  ContextMenu.prototype.constructor = ContextMenu;
-
-  ContextMenu.getCoords = function(elm) {
-    let box = elm.getBoundingClientRect();
-
-    return {
-      top: box.top,
-      left: box.left,
-      right: document.body.clientWidth - box.right,
-      bottom: document.body.clientHeight - box.bottom,
-      width: box.right - box.left,
-      height: box.bottom - box.top
-    }
-  }
-
-  ContextMenu.prototype.getNode = function() {
-    return Menu.prototype.getNode.call(this);
-  }
-
-  ContextMenu.prototype.render = function(whare = document.body) {
-    var container = document.createElement('div');
-    container.classList.add('contextmenu', 'hidden');
-    container.appendChild(Menu.prototype.getNode.call(this));
-    whare.appendChild(container);
-    return container;
-  }
-
-  ContextMenu.prototype.show = function(evt){
-    evt.preventDefault();
-    let menuNode = this.container;
-    
-    if (evt.type !== 'click') {
-      menuNode.classList.toggle('hidden');
-    } else {
-      menuNode.classList.add('hidden');
-    }
-    this.coodrs(evt);
-  }
-
-  ContextMenu.prototype.coodrs = function(evt){
-    evt.preventDefault();
-    let menuNode = this.container;
-    
-    let pos = {
-      'x': evt.x,
-      'y': evt.y
-    }
-    
-    menuNode.style.top = `${pos.y}px`;
-    menuNode.style.left = `${pos.x}px`;
-    let coords = this.constructor.getCoords;
-
-    console.log(`top: ${coords(menuNode).top}, right ${coords(menuNode).right}, bottom ${coords(menuNode).bottom}, left ${coords(menuNode).left}`);
-
-    if (coords(menuNode).right < 0) {
-      menuNode.style.left = `${pos.x - coords(menuNode).width}px`;
-      console.log('out right!');
-    }
-
-    if(coords(menuNode).bottom < 0) {
-      menuNode.style.top = `${pos.y - coords(menuNode).height}px`;
-      console.log('out bottom!');
-    }
-  }
-
-  ContextMenu.prototype.listener = function() {
-    let menu = ContextMenu.prototype.show.bind(this);
-    let menuCoords = ContextMenu.prototype.coodrs.bind(this);
-
-    document.body.addEventListener('click', menu, false);
-    document.body.addEventListener('contextmenu', menu, false);
-  }
-
-  new ContextMenu(['menu item 1', 'menu item 2', 'menu item 3']);
-});
+tripsNew.sort(createComporator('price.value', 'DESC'));
+console.log(`Sort DESC: ${tripsNew.map(item=>item.price.value)}`);
---
+++ build/js/my/trips.js
@@ -0,0 +1,76 @@
+export default [{
+  number: 'TEST - TEST2 - ATR005 Киев - Львов',
+  price: {
+    value: 620,
+    cents: 30,
+    currency: 'RUB',
+  },
+  'start-station': {
+    name: 'Центральный железнодорожный вокзал, автостанция Киев',
+    address: 'ул. С.Петлюри, 32 (платформа 34-37)',
+  },
+  'end-station': {
+    name: 'Автовокзал',
+    address: 'ул. Стрыйская 109 Платформа 3-7',
+  },
+  'carrier-name': 'ФЛП Бакал С.В.',
+  'time-in-road': '08:30',
+  'start-date': '2017-02-02',
+  'start-time': '13:30',
+  'start-city-name': 'Киев Ж/Д вокзал',
+  'end-date': '2017-02-02',
+  'end-time': '22:00',
+  'end-city-name': 'Львов',
+  'places-count': {
+    total: 18,
+    free: 8,
+  },
+}, {
+  number: 'TEST - TEST2 - ЕВК011 Киев - Львов',
+  price: {
+    value: 694,
+    cents: 74,
+    currency: 'RUB',
+  },
+  'start-station': {
+    name: 'Автостанция Дачная',
+    address: 'проспект Победы, 142',
+  },
+  'end-station': {
+    name: 'г. Львов, Стрийский автовокзал',
+    address: 'ул. Стрийская, 109',
+  },
+  'carrier-name': 'Евроклуб ООО',
+  'time-in-road': '07:30',
+  'start-date': '2017-02-02',
+  'start-time': '09:00',
+  'start-city-name': 'Киев',
+  'end-date': '2017-02-02',
+  'end-time': '16:30',
+  'end-city-name': 'Львов',
+  'places-count': {
+    total: 49,
+    free: 3,
+  },
+}, {
+  number: 'TEST - TEST2 - УВЖ555 Киев - Буковель',
+  price: {
+    value: 744,
+    cents: 36,
+    currency: 'RUB',
+  },
+  'carrier-name': 'Укрвояж ТОВ',
+  'time-in-road': '06:35',
+  'start-city-name': 'Киев',
+  'end-station': {
+    name: 'Ресторация Кумпель',
+    address: 'просп. Вячеслава Черновола, 2Б',
+  },
+  'end-date': '2017-02-03',
+  'end-time': '03:55',
+  'end-city-name': 'Львов',
+  'places-count': {
+    total: 18,
+    free: 8,
+  },
+}];
